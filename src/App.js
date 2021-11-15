2af81603fb59fe149ab6575480b87ec2f0207925
Alina Kasianova <avkasyanova@gmail.com>
2021-11-15 18:27:10 +0200

    localstorage added

--- src/App.js
+++ src/App.js
@@ -1,80 +1,8 @@
-// import React, { useState, useEffect, Component } from 'react';
-// import s from './App.module.css';
-// import shortid from 'shortid';
 import ContactList from './components/ContactList';
 import Form from './components/Form';
 import Filter from './components/Filter';
 
-// const useLocalStorage = (key, defaultValue) => {
-//   const [state, setState] = useState(() => {
-//     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-//   });
-
-//   useEffect(() => {
-//     localStorage.setItem(key, JSON.stringify(state));
-//   }, [key, state]);
-
-//   return [state, setState];
-// };
-
-// function App() {
-// const [filter, setFilter] = useState('');
-// const [contacts, setContacts] = useLocalStorage('contacts', [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ]);
-
-// const addContactHandler = (name, number) => {
-//   const newContact = {
-//     name,
-//     number,
-//     id: shortid.generate(),
-//   };
-
-// const duplicateContact = contacts.find(contact => contact.name === name);
-// if (duplicateContact) {
-//   alert(`${name} is already in contacts`);
-//   return;
-// }
-
-//   setContacts(state => [newContact, ...state]);
-// };
-
-// const changeFilter = e => {
-//   setFilter(e.currentTarget.value);
-// };
-
-// const getVisibleContacts = () => {
-//   const normalizedFilter = filter.toLowerCase();
-//   return contacts.filter(({ name }) =>
-//     name.toLowerCase().includes(normalizedFilter),
-//   );
-// };
-
-// const deleteContact = contactId => {
-//   setContacts(prevState => prevState.filter(({ id }) => id !== contactId));
-//   resetFilter();
-// };
-
-// const resetFilter = () => {
-//   setFilter('');
-// };
-
-// const visibleContacts = getVisibleContacts();
-
 function App() {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-
   // render() {
   return (
     <>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,41 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './contacts/contacts-reducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(contactsPersistConfig, reducer);
 
 const store = configureStore({
-  reducer: { contacts: reducer },
+  reducer: { contacts: persistedReducer },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
   devtools: process.env.NODE_ENV === 'development',
 });
-export default store;
+
+const persistor = persistStore(store);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { store, persistor };
 
 //VANILLA REDUX
 
